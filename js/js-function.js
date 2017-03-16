4fa9ae16f2237a9677a1d0e4e09533e9b5868a96
lukistr <luki_strike@abv.bg>
2017-03-16 13:33:21 +0200

    1

---
+++ js/js-function.js
@@ -0,0 +1,84 @@
+/**
+ * Created by Stanimir on 16.3.2017 г..
+ */
+
+/* When the user clicks on the button,
+ toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 5,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+    jQuery('.owl-prev').html('<i class="fa fa-arrow-left" aria-hidden="true"></i>');
+    jQuery('.owl-next').html('<i class="fa fa-arrow-right" aria-hidden="true"></i>');
+    $(".small-screen-menu").hover(
+        function () {
+            $(".menu ul").css(
+                {
+                    display: "block"
+                }
+            );
+        });
+});
+
+$(document).ready(function() {
+    $('#country').change(function() {
+        var current = $('#country').val();
+        if (current != 'null') {
+            $('#country').css('color','black');
+        } else {
+            $('#country').css('color','#B5A6B0');
+        }
+    });
+});
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(42.696552, 23.32601),
+        zoom: 12,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(42.696552, 23.32601),
+        map: map
+    });
+}
